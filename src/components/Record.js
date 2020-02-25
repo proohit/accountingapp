0ec9dc29aa5f23b2e27cceca8f0a613c04d709a7
Direnc Timur <direnc99@gmail.com>
2020-02-25 23:16:58 +0100

    record card

--- src/components/Record.js
+++ src/components/Record.js
@@ -1,6 +1,13 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
-import { Grid, Typography } from '@material-ui/core'
+import { Grid, Typography, Card, CardContent } from '@material-ui/core'
+
+const styles = {
+    card: {
+        minWidth: 300,
+        marginTop: 25
+    }
+}
 
 export class Record extends Component {
     static propTypes = {
@@ -10,10 +17,18 @@ export class Record extends Component {
         timestamp: PropTypes.object.isRequired
     }
 
+
     render() {
         return (
             <Grid key={this.props.id} item>
-                <Typography >{this.props.description}</Typography>
+                <Card style={styles.card}>
+                    <CardContent>
+                        <Typography variant="h5" component="h2">{this.props.timestamp}</Typography>
+                        <Typography variant="body2" component="p">{this.props.description}</Typography>
+                        <Typography style={{ color: this.props.value < 0 ? 'red' : 'gray' }} variant="body2" component="p">{this.props.value}</Typography>
+                    </CardContent>
+                </Card>
+                <Typography ></Typography>
             </Grid>
         )
     }

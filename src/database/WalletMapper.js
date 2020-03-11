7c9b1d6f70e0b18dcab9ee1206aaafb29f7074f0
Direnc Timur <direnc.timur@exxeta.com>
2020-03-11 11:17:20 +0100

    rewrite to async/await, delete wallet

--- src/database/WalletMapper.js
+++ src/database/WalletMapper.js
@@ -1,14 +1,13 @@
 const { con } = require('./database')
 const Wallet = require('./Wallet')
 
-byName = (walletName) => {
-    return new Promise((reject, resolve) => {
-
-        con.query(`SELECT * FROM Wallet WHERE name = '${walletName}'`, (err, res) => {
-            if (err) reject({ success: false, err, message: `Something went wrong retrieving wallet with name ${walletName}` })
-            else resolve({ success: true, message: res })
-        })
-    })
+byName = async (walletName) => {
+    try {
+        const wallet = await con.query(`SELECT * FROM Wallet WHERE name = '${walletName}'`)
+        return { success: true, message: wallet[0] }
+    } catch (error) {
+        return { sucess: false, error, message: "Error retrieving wallet" }
+    }
 }
 byUser = async (owner) => {
     try {
@@ -19,16 +18,30 @@ byUser = async (owner) => {
         return { success: false, message: error.message }
     }
 }
-create = (walletName, balance, owner) => {
-    return new Promise((resolve, reject) => {
+create = async (walletName, balance, owner) => {
+    try {
+        const created = await con.query(`INSERT INTO Wallet(name, balance, owner) VALUES('${walletName}',${balance},'${owner}') `);
+        const createdWallet = await byName(walletName);
+        return { success: true, message: createdWallet.message }
+    } catch (error) {
+        throw { success: false, message: error.message }
+    }
+}
 
-        con.query(`INSERT INTO Wallet(name, balance, owner) VALUES('${walletName}',${balance},'${owner}') `, (err, res) => {
-            if (err) reject({ success: false, err, message: `Something went wrong creating the wallet` })
-            else byName(walletName).then(res => resolve(res)).catch(err => reject(err))
-        })
-    })
+/**
+ * Deletes the provided wallet
+ * @param {string} walletName name of wallet
+ * @param {string} owner owner as username
+ */
+const deleteWallet = async (walletName, owner) => {
+    try {
+        con.query(`DELETE FROM Wallet WHERE name='${walletName}' AND owner='${owner}'`);
+        return { success: true, message: "deleted wallet" }
+    } catch (error) {
+        throw error;
+    }
 }
 
 module.exports = {
-    byName, create, byUser
+    byName, create, byUser, deleteWallet
 }
--- src/routes/walletRouter.js
+++ src/routes/walletRouter.js
@@ -2,51 +2,89 @@ const Router = require('koa-router')
 
 const database = require('../database/database');
 const walletMapper = require('../database/WalletMapper')
+const recordMapper = require('../database/RecordMapper')
 
 const router = new Router();
 
-router.post('/', async ctx => {
-    await database.verify(ctx.request).then(async decoded => {
-        await walletMapper.create(ctx.request.body.name, ctx.request.body.balance, decoded.username).then(res => {
-            ctx.response.type = 'application/json'
-            ctx.response.status = 201;
-            ctx.response.body = JSON.stringify(res)
-        }).catch(err => {
-            ctx.response.status = 400;
-            ctx.response.body = JSON.stringify(err);
-        })
-    }).catch(err => {
+router.use('/', async (ctx, next) => {
+    try {
+        const decoded = await database.verify(ctx.request);
+        ctx.state.token = decoded;
+        await next();
+        return;
+    } catch (error) {
+        ctx.response.type = 'application/json'
         ctx.response.status = 403;
-        ctx.response.body = JSON.stringify(err);
-    })
+        ctx.response.body = JSON.stringify(error)
+        return;
+    }
+
+})
+
+router.post('/', async ctx => {
+    const decoded = ctx.state.token;
+    try {
+        const res = await walletMapper.create(ctx.request.body.name, ctx.request.body.balance, decoded.username);
+        ctx.response.status = 201;
+        ctx.response.body = JSON.stringify(res)
+    } catch (error) {
+        ctx.response.status = 400;
+        ctx.response.body = JSON.stringify(error)
+        return;
+    }
 })
 
 router.get('/', async ctx => {
+    const decoded = ctx.state.token;
     try {
-        const decoded = await database.verify(ctx.request);
         const result = await walletMapper.byUser(decoded.username)
-
         ctx.response.type = 'application/json'
         ctx.response.status = 200;
         ctx.response.body = JSON.stringify(result)
-        return;
     } catch (error) {
+        ctx.response.type = 400;
         ctx.response.body = JSON.stringify(error.message)
+        return;
     }
 })
 
 router.get('/:name', async ctx => {
-    await database.verify(ctx.request).then(async decoded => {
-        await walletMapper.byName(ctx.params.name).then(data => {
-            ctx.response.type = 'application/json';
-            ctx.response.status = 200;
-            ctx.response.body = JSON.stringify(data);
-        })
-    }).catch(err => {
-        ctx.response.status = 403;
+    try {
+        const wallet = await walletMapper.byName(ctx.params.name);
+        ctx.response.type = 'application/json';
+        ctx.response.status = 200;
+        ctx.response.body = JSON.stringify(wallet);
+        return;
+    } catch (error) {
+        ctx.response.status = 400;
         ctx.response.body = JSON.stringify(err);
-    });
+        return;
+    }
 })
+
+router.delete('/:name', async ctx => {
+    try {
+        const recordsByUser = await recordMapper.allByUser(ctx.state.token.username)
+        recordsByUser.message
+            .filter(record => record.wallet === ctx.params.name)
+            .forEach(async record => {
+                await recordMapper.deleteRecord(record.id)
+            })
+
+        const res = await walletMapper.deleteWallet(ctx.params.name, ctx.state.token.username)
+        ctx.response.type = "application/json"
+        ctx.response.status = 200;
+        ctx.response.body = JSON.stringify(res)
+    } catch (error) {
+        ctx.response.type = "application/json"
+        ctx.response.status = 400;
+        ctx.response.body = JSON.stringify(error)
+        return;
+    }
+
+})
+
+
 
 
 module.exports = router.routes();

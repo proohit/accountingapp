c80ec2a969860cc444e5a048d6f2bdc6cf78815f
Direnc Timur <direnc99@gmail.com>
2020-04-15 16:54:33 +0200

    ReDOC Api documentation

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 import Koa from 'koa';
 import Router from 'koa-router';
-import cors from 'koa-cors'
-import parser from 'koa-bodyparser'
+import cors from 'koa-cors';
+import parser from 'koa-bodyparser';
+import { createReadStream } from 'fs';
 import config from '../config';
 
 import database = require('./database/database');
@@ -15,40 +16,45 @@ import walletRouter from './routes/wallets/walletRouter';
 app.use(parser());
 app.use(cors());
 
-router.use('/records', recordRouter)
-router.use('/wallets', walletRouter)
+router.use('/records', recordRouter);
+router.use('/wallets', walletRouter);
 
+router.post('/register', async (ctx) => {
+  try {
+    const res = await database.register(
+      ctx.request.body.username,
+      ctx.request.body.password
+    );
+    ctx.response.status = 201;
+    ctx.response.body = JSON.stringify(res);
+  } catch (error) {
+    console.log(error);
+    ctx.response.status = 400;
+    ctx.response.body = JSON.stringify(error);
+  }
+});
 
-router.post('/register', async ctx => {
-    try {
-        ctx.req
-        const res = await database.register(ctx.request.body.username, ctx.request.body.password)
-        ctx.response.status = 201;
-        ctx.response.body = JSON.stringify(res);
-    } catch (error) {
-        console.log(error);
-        ctx.response.status = 400;
-        ctx.response.body = JSON.stringify(error);
-    }
-})
+router.post('/login', async (ctx) => {
+  try {
+    const res = await database.login(ctx.request);
+    ctx.response.status = 201;
+    ctx.response.body = JSON.stringify(res);
+  } catch (error) {
+    ctx.response.status = 400;
+    ctx.response.body = JSON.stringify(error.message);
+  }
+});
 
-router.post('/login', async ctx => {
-    try {
-        const res = await database.login(ctx.request)
-        ctx.response.status = 201;
-        ctx.response.body = JSON.stringify(res);
-    } catch (error) {
-        ctx.response.status = 400;
-        ctx.response.body = JSON.stringify(error.message);
-    }
-})
-
+router.get('/docs', async (ctx, next) => {
+  ctx.type = 'html';
+  ctx.body = createReadStream('docs/index.html');
+});
 
 app.use(router.allowedMethods({ throw: true }));
-app.use(router.routes())
+app.use(router.routes());
 try {
-    app.listen(config.backendPort);
-    console.log(`running on port ${config.backendPort}`);
+  app.listen(config.backendPort);
+  console.log(`running on port ${config.backendPort}`);
 } catch (e) {
-    console.log(e);
+  console.log(e);
 }

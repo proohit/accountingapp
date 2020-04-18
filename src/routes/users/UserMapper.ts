409bdcb62101f068faa3631ee09fe33a691f7180
Direnc Timur <direnc99@gmail.com>
2020-04-18 16:00:04 +0200

    init DB workflow

--- src/routes/users/UserMapper.ts
+++ src/routes/users/UserMapper.ts
@@ -1,14 +1,32 @@
-const {con} = require('./database')
+import { con } from '../../database/database';
 
-byName = (username) => {
-    return new Promise((resolve, reject) => {
-        con.query(`SELECT * FROM User WHERE username=${username};`, (err, data) => {
-            if (err) reject(err);
-            resolve(data);
-        });
-    })
-}
+export const byName = async (username: string) => {
+  try {
+    const user = await con.query(
+      `SELECT * FROM User WHERE username='${username}';`
+    );
+    return user;
+  } catch (error) {
+    console.log(`error at UserMapper byName:`);
+    console.log(error);
+  }
+};
 
-module.exports = {
-    byName
-}
+export const createTable = async () => {
+  try {
+    const sql = `CREATE TABLE \`User\` (
+            \`username\` varchar(25) COLLATE utf8mb4_unicode_ci NOT NULL PRIMARY KEY,
+            \`password\` varchar(64) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+            \`private_key\` varchar(64) COLLATE utf8mb4_unicode_ci DEFAULT NULL
+          ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;`;
+    const res = await con.query(sql);
+  } catch (error) {}
+};
+
+export const createIndices = async () => {
+  try {
+    const sql = `ALTER TABLE \`User\`
+      ADD PRIMARY KEY (\`username\`);`;
+    const res = await con.query(sql);
+  } catch (error) {}
+};

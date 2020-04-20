6392f9badd654f3da74c6fe18ac46a3997143da8
Direnc Timur <direnc99@gmail.com>
2020-04-20 20:18:17 +0200

    removed web component attempt

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import ReactWebComponent from 'react-web-component';
+// import ReactWebComponent from 'react-web-component';
 
-//ReactDOM.render(<App />, document.getElementById('root'));
-ReactWebComponent.create(<App/>, 'my-app',false)
+ReactDOM.render(<App />, document.getElementById('root'));
+// ReactWebComponent.create(<App/>, 'my-app',false)

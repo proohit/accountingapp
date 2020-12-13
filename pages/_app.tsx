d9c236d81b5000a3a3623ef693d39885fbc55cfe
proohit <direnc99@gmail.com>
2020-12-14 00:15:03 +0100

    AppBar offset

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,31 @@
-import React from 'react';
+import React, { FunctionComponent } from 'react';
 import '../styles/globals.css';
 
 import type { AppProps } from 'next/app';
 import Providers from '../src/shared/components/Providers';
 import Authenticated from '../src/authentication/components/Authenticated';
+import { NavigationBar } from '../src/shared/components/NavigationBar';
+import { makeStyles } from '@material-ui/core';
 
-function MyApp({ Component, pageProps }: AppProps) {
+const styles = makeStyles((theme) => ({
+  toolbarOffset: {
+    ...theme.mixins.toolbar,
+  },
+}));
+
+const MyApp: FunctionComponent<AppProps> = ({ Component, pageProps }) => {
+  const classes = styles();
   return (
     <Providers>
       <Authenticated>
-        <Component {...pageProps} />
+        <NavigationBar />
+        <main>
+          <div className={classes.toolbarOffset} />
+          <Component {...pageProps} />
+        </main>
       </Authenticated>
     </Providers>
   );
-}
+};
 
 export default MyApp;

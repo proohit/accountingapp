3108766f93e63a72b8ce07b07bd60afa0c7adc86
proohit <direnc99@gmail.com>
2021-01-10 00:53:07 +0100

    updated config templates

--- configTemplate.ts
+++ configTemplate.ts
@@ -1,11 +1,5 @@
 export default {
-    database: '',
-    user: '',
-    password: '',
     secret: '',
     backendPort: 3002,
-    host: 'localhost',
-    port: 3306,
-    tables: ['record', 'wallet', 'user', 'category'],
     loggingLevel: 'info',
 };

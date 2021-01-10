420333096b6af317366a4e28e38b985c9515e223
proohit <direnc99@gmail.com>
2021-01-10 01:44:59 +0100

    Cleaned Repositories

--- src/category/repositories/CategoryRepository.ts
+++ src/category/repositories/CategoryRepository.ts
@@ -1,20 +1,5 @@
 import { EntityRepository, Repository } from 'typeorm';
 import { Category } from '../../entity/Category';
-import { User } from '../../entity/User';
-import { ResourceNotAllowed } from '../../shared/models/Errors';
-import { CategoryNotFound } from '../models/Errors';
 
 @EntityRepository(Category)
-export class CategoryRepository extends Repository<Category> {
-    async getByIdIfAllowed(id: Category['id'], username: User['username']): Promise<Category> {
-        const category = await this.findOne(id);
-
-        if (!category) {
-            throw new CategoryNotFound();
-        }
-        if (category.ownerUsername !== username) {
-            throw new ResourceNotAllowed();
-        }
-        return category;
-    }
-}
+export class CategoryRepository extends Repository<Category> {}
--- src/record/repositories/RecordRepository.ts
+++ src/record/repositories/RecordRepository.ts
@@ -1,21 +1,5 @@
 import { EntityRepository, Repository } from 'typeorm';
 import { Record } from '../../entity/Record';
-import { User } from '../../entity/User';
-import { RecordNotFound } from '../models/Errors';
-import { ResourceNotAllowed } from '../../shared/models/Errors';
 
 @EntityRepository(Record)
-export class RecordRepository extends Repository<Record> {
-    async getByIdIfAllowed(id: Record['id'], username: User['username']): Promise<Record> {
-        const record = await this.findOne(id);
-        if (!record) {
-            throw new RecordNotFound();
-        }
-
-        if (record.ownerUsername !== username) {
-            throw new ResourceNotAllowed();
-        }
-
-        return record;
-    }
-}
+export class RecordRepository extends Repository<Record> {}

81d293bcd4f76bda50c00ab753893dbffc8c92a7
proohit <direnc99@gmail.com>
2021-03-01 21:38:29 +0100

    AW-27 - added greeting to registration

--- src/authentication/components/RegisterForm.tsx
+++ src/authentication/components/RegisterForm.tsx
@@ -1,13 +1,17 @@
 import {
   Button,
   Container,
+  Dialog,
+  DialogContent,
+  DialogTitle,
   Grid,
   makeStyles,
   TextField,
   Typography,
 } from '@material-ui/core';
 import { useRouter } from 'next/dist/client/router';
-import React, { FunctionComponent, useState } from 'react';
+import Link from 'next/link';
+import React, { FunctionComponent, useEffect, useState } from 'react';
 import { useAuthentication } from '../hooks/useAuthentication';
 import { AUTHENTICATION_API } from '../services/AuthenticationApi';
 
@@ -31,9 +35,14 @@ export const RegisterForm: FunctionComponent = () => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [email, setEmail] = useState('');
+  const [registerGreeting, setRegisterGreeting] = useState(false);
   const classes = useStyles();
   const router = useRouter();
-  const { login } = useAuthentication();
+  const {
+    login,
+    authenticated,
+    username: loggedInUsername,
+  } = useAuthentication();
 
   const handleSubmit = async () => {
     const createdUser = await AUTHENTICATION_API.register(
@@ -42,65 +51,98 @@ export const RegisterForm: FunctionComponent = () => {
       email
     );
     login(createdUser.username);
-    router.push('/home');
+    setRegisterGreeting(true);
   };
 
+  useEffect(() => {
+    if (authenticated) {
+      router.push('/home');
+    }
+  }, []);
+
   return (
-    <Grid container direction="row" justify="center">
-      <Container maxWidth="xs">
-        <div className={classes.paper}>
-          <Typography component="h1" variant="h5">
-            Register
+    <>
+      <Dialog open={registerGreeting}>
+        <DialogTitle>
+          <Typography>Welcome!</Typography>
+        </DialogTitle>
+        <DialogContent>
+          <Typography variant="h5">
+            Hi, <b>{loggedInUsername}</b>!
           </Typography>
-          <div className={classes.form}>
-            <TextField
-              variant="outlined"
-              margin="normal"
-              required
-              fullWidth
-              id="username"
-              label="Username"
-              name="username"
-              autoComplete="username"
-              autoFocus
-              onChange={(event) => setUsername(event.target.value)}
-            />
-            <TextField
-              variant="outlined"
-              margin="normal"
-              required
-              fullWidth
-              id="username"
-              label="Email"
-              name="email"
-              autoComplete="email"
-              onChange={(event) => setEmail(event.target.value)}
-            />
-            <TextField
-              variant="outlined"
-              margin="normal"
-              required
-              fullWidth
-              name="password"
-              label="Password"
-              type="password"
-              id="password"
-              autoComplete="current-password"
-              onChange={(event) => setPassword(event.target.value)}
-            />
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              color="primary"
-              className={classes.submit}
-              onClick={handleSubmit}
-            >
+          <Typography>
+            We are happy to see you here. To be able to track your records, you
+            will need to create a wallet. Go to the{' '}
+            <Link href="/wallets" passHref>
+              <Button
+                size="small"
+                variant="outlined"
+                color="primary"
+                component="a"
+              >
+                Wallets page
+              </Button>
+            </Link>{' '}
+            to create your first wallet.
+          </Typography>
+        </DialogContent>
+      </Dialog>
+      <Grid container direction="row" justify="center">
+        <Container maxWidth="xs">
+          <div className={classes.paper}>
+            <Typography component="h1" variant="h5">
               Register
-            </Button>
+            </Typography>
+            <div className={classes.form}>
+              <TextField
+                variant="outlined"
+                margin="normal"
+                required
+                fullWidth
+                id="username"
+                label="Username"
+                name="username"
+                autoComplete="username"
+                autoFocus
+                onChange={(event) => setUsername(event.target.value)}
+              />
+              <TextField
+                variant="outlined"
+                margin="normal"
+                required
+                fullWidth
+                id="email"
+                label="Email"
+                name="email"
+                autoComplete="email"
+                onChange={(event) => setEmail(event.target.value)}
+              />
+              <TextField
+                variant="outlined"
+                margin="normal"
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+                onChange={(event) => setPassword(event.target.value)}
+              />
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                color="primary"
+                className={classes.submit}
+                onClick={handleSubmit}
+              >
+                Register
+              </Button>
+            </div>
           </div>
-        </div>
-      </Container>
-    </Grid>
+        </Container>
+      </Grid>
+    </>
   );
 };

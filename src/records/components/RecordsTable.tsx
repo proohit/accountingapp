0b0f94c9e1edc892c1a045b046abf54756da834b
proohit <direnc99@gmail.com>
2021-03-01 01:02:24 +0100

    AW-4 - Added no records text and loading indicator

--- src/records/components/RecordsTable.tsx
+++ src/records/components/RecordsTable.tsx
@@ -2,19 +2,21 @@ import { Grid, Paper, Table, TableContainer, Toolbar } from '@material-ui/core';
 import React, { FunctionComponent } from 'react';
 
 interface RecordsTableProps {
-  toolbar: JSX.Element;
-  controls: JSX.Element;
+  toolbar?: JSX.Element;
+  controls?: JSX.Element;
 }
 export const RecordsTable: FunctionComponent<RecordsTableProps> = (props) => {
   const { toolbar, children, controls } = props;
 
   return (
     <Paper>
-      <Toolbar>
-        <Grid container item direction="row" alignContent="center">
-          {toolbar}
-        </Grid>
-      </Toolbar>
+      {toolbar && (
+        <Toolbar>
+          <Grid container item direction="row" alignContent="center">
+            {toolbar}
+          </Grid>
+        </Toolbar>
+      )}
       <TableContainer>
         <Table>{children}</Table>
       </TableContainer>

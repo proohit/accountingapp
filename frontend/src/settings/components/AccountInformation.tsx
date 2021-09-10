f2913cc2f84d6a9d38c88b90ae15c2c8369640b0
proohit <direnc99@gmail.com>
2021-09-11 00:55:20 +0200

    fix error on settings

--- frontend/src/settings/components/AccountInformation.tsx
+++ frontend/src/settings/components/AccountInformation.tsx
@@ -1,13 +1,15 @@
-import { Typography } from '@material-ui/core';
+import { LinearProgress, Typography } from '@material-ui/core';
 import React from 'react';
 import { useAuthentication } from '../../authentication/hooks/useAuthentication';
 
 export const AccountInformation: React.FunctionComponent = () => {
-  const { user } = useAuthentication();
-  return (
+  const { user, isLoginLoading } = useAuthentication();
+  return isLoginLoading ? (
+    <LinearProgress />
+  ) : (
     <>
-      <Typography variant="h6">Username: {user.username}</Typography>
-      <Typography variant="h6">Email: {user.email}</Typography>
+      <Typography variant="h6">Username: {user?.username}</Typography>
+      <Typography variant="h6">Email: {user?.email}</Typography>
     </>
   );
 };

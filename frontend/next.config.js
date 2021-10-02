b0aba5d655f136d426fc45f74dc5087d2e95b180
proohit <direnc99@gmail.com>
2021-10-02 18:22:22 +0200

    Bump version 0.9.0

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,9 +1,13 @@
-const withPWA = require('next-pwa');
+// const withPWA = require('next-pwa');
 
-module.exports = withPWA({
-  pwa: {
-    dest: 'public',
-    disable: process.env.NODE_ENV === 'development',
-  },
+// module.exports = withPWA({
+//   pwa: {
+//     dest: 'public',
+//     disable: process.env.NODE_ENV === 'development',
+//   },
+//   trailingSlash: true,
+// });
+
+module.exports = {
   trailingSlash: true,
-});
+};

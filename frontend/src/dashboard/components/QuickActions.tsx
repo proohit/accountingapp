41a5143889c98ae4a833e451bc4e1108176dd768
proohit <direnc99@gmail.com>
2021-11-06 16:21:42 +0100

    implemented quickactions

--- frontend/src/dashboard/components/QuickActions.tsx
+++ frontend/src/dashboard/components/QuickActions.tsx
@@ -1,14 +1,26 @@
-import { Button, Grid, Toolbar } from '@material-ui/core';
+import { Button, Grid } from '@material-ui/core';
 import { AddBox } from '@material-ui/icons';
-import * as React from 'react';
+import { useRecoilState } from 'recoil';
+import { addRecordDialogState } from '../../records/hooks/recordsDialogsState';
+import { addWalletDialogState } from '../../wallets/hooks/walletDialogsState';
 
 export const QuickActions = () => {
+  const [, setAddRecordsDialog] = useRecoilState(addRecordDialogState);
+  const [, setAddWalletDialog] = useRecoilState(addWalletDialogState);
   return (
     <Grid container direction="row">
-      <Button color="primary" startIcon={<AddBox />}>
+      <Button
+        color="primary"
+        startIcon={<AddBox />}
+        onClick={() => setAddRecordsDialog({ open: true })}
+      >
         Add Record
       </Button>
-      <Button color="primary" startIcon={<AddBox />}>
+      <Button
+        color="primary"
+        startIcon={<AddBox />}
+        onClick={() => setAddWalletDialog({ open: true })}
+      >
         Add Wallet
       </Button>
     </Grid>

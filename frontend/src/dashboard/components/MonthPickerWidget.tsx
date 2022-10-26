2e5addcb023e4bf582403960b244bcd8aa1f2247
proohit <direnc99@gmail.com>
2022-10-26 15:36:53 +0200

    feat: next and prev month in monthpicker

--- frontend/src/dashboard/components/MonthPickerWidget.tsx
+++ frontend/src/dashboard/components/MonthPickerWidget.tsx
@@ -1,10 +1,11 @@
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
-import { TextField } from '@mui/material';
+import { Grid, IconButton, TextField, Tooltip } from '@mui/material';
 import { Dayjs } from 'dayjs';
 import { FunctionComponent, useState } from 'react';
 import { DateableWidget } from '../models/DateableWidget';
 import { EditableWidgetProps } from '../models/EditableWidgetProps';
 import Widget from './Widget';
+import { ArrowLeft, ArrowRight } from '@mui/icons-material';
 
 export const MonthPickerWidget: FunctionComponent<
   EditableWidgetProps &
@@ -14,21 +15,56 @@ export const MonthPickerWidget: FunctionComponent<
 > = (props) => {
   const { date, setCurrentDate, ...rest } = props;
   const [selectedDate, setSelectedDate] = useState<Dayjs>(date);
+
+  const updateDate = (acceptedDate: Dayjs): void => {
+    setSelectedDate(acceptedDate);
+    if (acceptedDate && acceptedDate.isValid()) {
+      setCurrentDate(acceptedDate);
+    }
+  };
+
   return (
     <Widget {...rest} xs={6}>
-      <DatePicker
-        renderInput={(inputProps) => <TextField {...inputProps} />}
-        views={['year', 'month']}
-        onChange={(acceptedDate) => {
-          setSelectedDate(acceptedDate);
-          if (acceptedDate && acceptedDate.isValid()) {
-            setCurrentDate(acceptedDate);
-          }
-        }}
-        openTo="month"
-        value={selectedDate}
-        label="Month"
-      />
+      <Grid container alignItems="center">
+        <DatePicker
+          renderInput={(inputProps) => (
+            <TextField
+              {...inputProps}
+              InputProps={{
+                ...inputProps.InputProps,
+                endAdornment: (
+                  <>
+                    <Tooltip title="Previous month">
+                      <IconButton
+                        onClick={() => {
+                          updateDate(selectedDate.subtract(1, 'month'));
+                        }}
+                      >
+                        <ArrowLeft />
+                      </IconButton>
+                    </Tooltip>
+                    <Tooltip title="Next month">
+                      <IconButton
+                        onClick={() => {
+                          updateDate(selectedDate.add(1, 'month'));
+                        }}
+                      >
+                        <ArrowRight />
+                      </IconButton>
+                    </Tooltip>
+                    {inputProps.InputProps.endAdornment}
+                  </>
+                ),
+              }}
+            />
+          )}
+          views={['year', 'month']}
+          onChange={updateDate}
+          openTo="month"
+          value={selectedDate}
+          label="Month"
+        />
+      </Grid>
     </Widget>
   );
 };

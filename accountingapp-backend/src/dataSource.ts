c66c08038554c47572e0daaa2d4d64c37b5f7a8e
proohit <direnc99@gmail.com>
2022-10-28 16:33:54 +0200

    fix: data source options

--- accountingapp-backend/src/dataSource.ts
+++ accountingapp-backend/src/dataSource.ts
@@ -1,16 +1,17 @@
 import { DataSource } from 'typeorm';
 import * as dotenv from 'dotenv';
 dotenv.config();
+
 const dataSource = new DataSource({
-  type: 'mysql',
   url: process.env.DATABASE_URL,
+  type: 'mysql',
   synchronize: false,
   logging: false,
   migrationsRun: true,
   timezone: 'Z',
-  entities: ['dist/src/**/entities/*{.ts,.js}'],
-  migrations: ['dist/src/migration/**/*{.ts,.js}'],
-  subscribers: ['dist/src/subscriber/**/*{.ts,.js}'],
+  entities: ['dist/**/entities/*{.ts,.js}'],
+  migrations: ['dist/migration/*.js'],
+  subscribers: ['src/subscriber/**/*{.ts,.js}'],
 });
 
 export default dataSource;

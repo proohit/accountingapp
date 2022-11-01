5e24746f02994b852916dbe4e2f4a167e553370a
proohit <direnc99@gmail.com>
2022-11-01 21:48:37 +0100

    feat: introduce shared package

--- update-version.sh
+++ update-version.sh
@@ -4,6 +4,8 @@ cd frontend
 npm --no-git-tag-version version $VERSION
 cd ../accountingapp-backend
 npm --no-git-tag-version version $VERSION
+cd ../shared
+npm --no-git-tag-version version $VERSION
 cd ..
 git add .
 git commit -m "Bump version $1"

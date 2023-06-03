6ccfe37c1ea5ebabe0340da277d5b342c6cd11a5
proohit <direnc99@gmail.com>
2023-06-03 03:46:14 +0200

    fix: use correct props for date pickers

--- frontend/src/dashboard/components/MonthPickerWidget.tsx
+++ frontend/src/dashboard/components/MonthPickerWidget.tsx
@@ -1,11 +1,11 @@
+import { ArrowLeft, ArrowRight } from '@mui/icons-material';
+import { Grid, IconButton, Tooltip } from '@mui/material';
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
-import { Grid, IconButton, TextField, Tooltip } from '@mui/material';
 import { Dayjs } from 'dayjs';
 import { FunctionComponent, useState } from 'react';
 import { DateableWidget } from '../models/DateableWidget';
 import { EditableWidgetProps } from '../models/EditableWidgetProps';
 import Widget from './Widget';
-import { ArrowLeft, ArrowRight } from '@mui/icons-material';
 
 export const MonthPickerWidget: FunctionComponent<
   EditableWidgetProps &
@@ -25,32 +25,32 @@ export const MonthPickerWidget: FunctionComponent<
 
   return (
     <Widget {...rest} xs={6}>
-      <Grid container alignItems="center"><Tooltip title="Previous month">
-        <IconButton
-          onClick={() => {
-            updateDate(selectedDate.subtract(1, 'month'));
-          }}
-        >
-          <ArrowLeft />
-        </IconButton>
-      </Tooltip>
+      <Grid container alignItems="center">
+        <Tooltip title="Previous month">
+          <IconButton
+            onClick={() => {
+              updateDate(selectedDate.subtract(1, 'month'));
+            }}
+          >
+            <ArrowLeft />
+          </IconButton>
+        </Tooltip>
         <DatePicker
-          renderInput={(inputProps) => <TextField {...inputProps} />}
           views={['year', 'month']}
           onChange={updateDate}
           openTo="month"
           value={selectedDate}
           label="Month"
-        /> 
-      <Tooltip title="Next month">
-        <IconButton
-          onClick={() => {
-            updateDate(selectedDate.add(1, 'month'));
-          }}
-        >
-          <ArrowRight />
-        </IconButton>
-      </Tooltip>
+        />
+        <Tooltip title="Next month">
+          <IconButton
+            onClick={() => {
+              updateDate(selectedDate.add(1, 'month'));
+            }}
+          >
+            <ArrowRight />
+          </IconButton>
+        </Tooltip>
       </Grid>
     </Widget>
   );
